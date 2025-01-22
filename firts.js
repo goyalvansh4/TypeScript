d73b893f5557b9a58c0d91546db946fb1d1e548b
goyalvansh4 <vanshgupta7668@gmail.com>
2025-01-22 21:33:15 +0530

    first commit

---
+++ firts.js
@@ -0,0 +1,5 @@
+var username = "Saurav";
+var age = 25;
+var isLearning = true;
+var skills = ["JavaScript", "TypeScript"];
+console.log(username, age, isLearning, skills);
---
+++ firts.ts
@@ -0,0 +1,6 @@
+let username: string = "Saurav";
+let age: number = 25;
+let isLearning: boolean = true;
+let skills: string[] = ["JavaScript", "TypeScript"];
+
+console.log(username,age,isLearning,skills);
